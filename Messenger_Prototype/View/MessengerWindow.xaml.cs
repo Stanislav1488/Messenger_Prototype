81b0e882214422a667289cdf3977f6ae58558ace
Stan <stanislavbu8@gmail.com>
2026-02-20 23:16:35 +0700

    refactor: переименовать MainWindow в MessengerWindow

--- Messenger_Prototype/View/MessengerWindow.xaml.cs
+++ Messenger_Prototype/View/MessengerWindow.xaml.cs
@@ -18,9 +18,9 @@ namespace Messenger_Prototype.View
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MessengerWindow : Window
     {
-        public MainWindow()
+        public MessengerWindow()
         {
             InitializeComponent();
         }
