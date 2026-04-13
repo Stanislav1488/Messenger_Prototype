c1b07aa7982e121cd17ef4157853eb14630a50ba
Stan <stanislavbu8@gmail.com>
2026-04-13 21:31:27 +0700

    feat: добавлены модели данных Chat, Message, User

---
+++ Messenger_Prototype/Model/Chat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger_Prototype.Model
+{
+    public class Chat
+    {
+        public User Partner { get; set; }
+        public List<Message> Messages { get; set; }
+        public string LastMessage { get; set; }
+    }
+}
---
+++ Messenger_Prototype/Model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger_Prototype.Model
+{
+    public class Message
+    {
+        public DateTime Timestamp { get; set; }
+        public string Text { get; set; }
+        public bool IsOwn { get; set; }
+    }
+}
