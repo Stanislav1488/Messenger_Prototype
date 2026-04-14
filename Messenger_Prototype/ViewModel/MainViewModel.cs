ac2902e74f74ea6c76c8abfcbb5aff99f44019c3
Stan <stanislavbu8@gmail.com>
2026-04-15 02:20:41 +0700

    feat: добавить MainViewModel

---
+++ Messenger_Prototype/ViewModel/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messenger_Prototype.Model;
+
+namespace Messenger_Prototype.ViewModel
+{
+    internal class  MainViewModel : BaseViewModel
+    {
+        public ObservableCollection<Chat> Chats { get; set; }
+
+        private Chat _selectedChat;
+        public Chat selectedChat
+        {
+            get { return _selectedChat; }
+            set
+            {
+                _selectedChat = value;
+                OnPropertyChanged(nameof(selectedChat));
+            }
+        }
+    }
+}
