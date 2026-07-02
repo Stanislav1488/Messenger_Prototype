63767ca70d650e8602c8b2cdd795ce18627a524e
Stan <stanislavbu8@gmail.com>
2026-07-02 22:48:13 +0700

    feat: создан класс ChatHub c методом Send для рассылки сообщений

---
+++ Messenger_Prototype/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Messenger_Prototype.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string userName)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", message, userName);
+        }
+    }
+}
