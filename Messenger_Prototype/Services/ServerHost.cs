6b3aadb0c4d90e9010e99d07fe232bb6a6b3354c
Stan <stanislavbu8@gmail.com>
2026-07-07 19:09:17 +0700

    feat: добавил ServerHost с настройкой SignalR

---
+++ Messenger_Prototype/Services/ServerHost.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+using Messenger_Prototype.Hubs;
+
+namespace Messenger_Prototype.Services
+{
+    public class ServerHost
+    {
+        private IHost host;
+
+        public async Task StartAsync()
+        {
+            var builder = Host.CreateDefaultBuilder();
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddSignalR();
+            });
+
+            builder.ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseUrls("http://localhost:5000");
+                webBuilder.Configure(app =>
+                {
+                    app.UseRouting();
+                    app.UseEndpoints(endpoints =>
+                    {
+                        endpoints.MapHub<ChatHub>("/chatHub");
+                    });
+                });
+            });
+
+            host = builder.Build();
+
+            await host.StartAsync();
+        }
+
+        public async Task StopAsync()
+        {
+            if(host != null)
+            {
+                await host.StopAsync();
+                host.Dispose();
+            }
+        }
+    }
+}
