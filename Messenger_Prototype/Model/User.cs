fc27519bd383683499b8ab49095911141c563b3c
Stan <stanislavbu8@gmail.com>
2026-07-21 05:48:06 +0700

    feat: добавил класс User

---
+++ Messenger_Prototype/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messenger_Prototype.Model
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+    }
+}
