f01b1d053c1476c060a6c4775d0d6edbb7d14cfe
Stan <stanislavbu8@gmail.com>
2026-07-22 07:49:46 +0700

    feat: Создал и подключил к LoginWindow класс LoginViewModel

---
+++ Messenger_Prototype/ViewModel/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Messenger_Prototype.View;
+
+namespace Messenger_Prototype.ViewModel
+{
+    public class LoginViewModel : BaseViewModel
+    {
+
+    }
+}
